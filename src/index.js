a4047713bc12aad84c423b7327ca69f2268103f7
Ivan Markov <30551895+IvMarKon@users.noreply.github.com>
2018-03-07 20:35:20 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import Rx from 'rx';
+import * as Rx from 'rxjs/Rx';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -11,23 +11,33 @@ class Phonebook extends Component{
     }
 
     componentDidMount(){
-      Rx.Observable.fromEvent(window, 'scroll')
-      .throttle(1000)
-      //.map((data) => {return data})
-      .subscribe(function(e){console.log(e);});
+      // Rx.Observable.fromEvent(window, 'scroll')
+      // .throttle(1000)
+      // //.map((data) => {return data})
+      // .subscribe(function(e){console.log(e);});
     
-      Rx.Observable.fromEvent(this.scrl, 'click')
-      .subscribe(Rx.Observer.create(
-        function(value) {
-          console.log(value);
-        },
-        function(err) {
-          console.error(err);
-        },
-        function() {
-          console.log('Completed');
-        }
-      ));
+      // Rx.Observable.fromEvent(this.scrl, 'click')
+      // .subscribe(Rx.Observer.create(
+      //   function(value) {
+      //     console.log(value);
+      //   },
+      //   function(err) {
+      //     console.error(err);
+      //   },
+      //   function() {
+      //     console.log('Completed');
+      //   }
+      // ));
+
+      var s3 = Rx.Observable.of(1,2,3);
+      var sbs3 = s3.subscribe(x => console.log(x));
+
+      var observable2 = Rx.Observable.interval(1000);
+      var subscription2 = observable2.subscribe(x => console.log(x));
+      setTimeout(function(){
+        subscription2.unsubscribe();
+      },5000);
+
     }
 
     render(){  
